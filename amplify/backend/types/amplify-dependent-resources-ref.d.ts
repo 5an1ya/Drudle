a216c7a05699ff4fed3cb74c31ffc8de54e89117
Becky <rreyes@cloudwell.io>
2024-07-21 19:34:25 -0400

    Update GraphQL api files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "drudle": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
