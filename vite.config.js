426e40804c16c10fac0d71dbc0d68a800093d258
Becky <rreyes@cloudwell.io>
2024-08-02 12:33:11 -0400

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-const { defineConfig } = require('vite');
-const react = require('@vitejs/plugin-react');
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-module.exports = defineConfig({
+export default defineConfig({
   plugins: [react()],
 });
