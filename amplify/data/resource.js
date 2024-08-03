0e8d36dd30196613b990ea93f86ab60a3e5f0931
Becky <rreyes@cloudwell.io>
2024-08-03 15:06:46 -0400

    amplify/data/resource.js syntax correction

--- amplify/data/resource.js
+++ amplify/data/resource.js
@@ -6,7 +6,7 @@ const schema = a.schema({
     plantNickname: a.string(),
     scientificName: a.string(),
     birthdate: a.date(),
-    tasks: a.array(a.string()),
+    tasks: a.stringList(),
     plantPhoto: a.string(), // URL or file path for the photo
   })
   .authorization((allow) => [allow.publicApiKey()])
